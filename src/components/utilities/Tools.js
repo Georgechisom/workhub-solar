a23ac6a2b5d3c081751fe08140d0b49350a2baf0
Georgechisom <georgechipaul@gmail.com>
2025-05-19 01:47:22 +0100

    app updated

--- src/components/utilities/Tools.js
+++ src/components/utilities/Tools.js
@@ -3,7 +3,7 @@ import WhatsApp from "../../images/whatsapp.png";
 
 const Tools = () => {
 
-    const WhatsAppLink = "https://wa.me/+2349066290201?text=Hello,%20I'm%20from%20workhub%20solar%20website%20I'd%20have%20a%20job%20for%20you";
+    const WhatsAppLink = "https://wa.me/+2349066290201?text=Hello,%20I'm%20from%20workhub%20solar%20website%20I'd%20have%20a%20job%20for%20you%20.%20My%20name%20is%20";
 
   return (
     <div>
